2d62285157c4624bbbffa3ee660afb924a72a21b
minwoonggi <min20000416@naver.com>
2022-07-05 23:54:39 +0900

    [22.07.05] 장고가 media 파일을 다루는 방법

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 # 등록법 1
@@ -14,10 +15,15 @@ from .models import Post
 # 등록법 3
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
+    list_display = ['id', 'photo_tag', 'message', 'message_length', 'is_public', 'created_at', 'updated_at']
     list_display_links = ['message']
     list_filter = ['created_at', 'is_public']
     search_fields = ['message']
+    
+    def photo_tag(self,post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}" />')
+        return None
     
     def message_length(self, post): # admin이 알아서 호출해주는 post
         return f"{len(post.message)} 글자"
